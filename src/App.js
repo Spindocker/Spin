6e2b8112c56697c66fed32f9d1578d7db3e95c13
jmangz <echou1@binghamton.edu>
2018-05-08 12:01:26 -0400

    cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -4,40 +4,29 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      containers: []
+      containers: [],
     };
   }
 
   componentDidMount() {
     fetch('/psa', {
       method: 'GET',
-      headers: ({
-        'Content-Type': 'application/json'
-      })
-    }).then(res => {
-      return res.json()
-    }).then(data => {
-      console.log(data)
-      this.setState({
-        containers: data
-      })
-      console.log(this.state.containers)
-    })
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(res => res.json())
+      .then((data) => {
+        this.setState({
+          containers: data,
+        });
+      });
   }
-
-  // componentDidUpdate() {
-  //   console.log(this.state)
-  // }
 
   render() {
     return (
       <div>
         <h1>Compose GUI</h1>
-        {
-          this.state.containers.map(container => {
-            return <p>{container['CONTAINER ID']}</p>
-          })
-        }
+        ${this.state.containers}
       </div>
     );
   }
