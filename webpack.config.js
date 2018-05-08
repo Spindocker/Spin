a0c54875e256380e64d3b480dbf5b6e1f3bbddba
jmangz <echou1@binghamton.edu>
2018-05-08 15:34:13 -0400

    added css rule to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,13 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
-      }
-    ]
-  }
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
