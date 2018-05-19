4372abb185893b11fc8b3fa92e9867a77ca42599
Paul Kassar <paulkassar@gmail.com>
2018-05-19 16:56:23 -0400

    Revert "Container and toolbar cleanup"

--- src/Button.js
+++ src/Button.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Button.css';
 
 const Button = ({ name, click}) => (
-  < div className = "button" >
-    <a  onClick={click} >{name}</a>
+  <div>
+    <button className="button" onClick={click} >{name}</button>
   </div>
 );
+
 
 export default Button;
