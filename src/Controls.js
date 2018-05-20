98ec2fc00822ca470a10b5321ca1f47df3abd565
jmangz <echou1@binghamton.edu>
2018-05-19 20:46:22 -0400

    Added saved directories button

--- src/Controls.js
+++ src/Controls.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from './Button';
 import './Controls.css';
 
-const Controls = ({ ps, psa, dcup, stop, fp, open, dcdwn }) => (
+const Controls = ({ ps, psa, dcup, stop, fp, open, dcdwn, directories }) => (
   <div id="controlsBar">
     <button id="openFolder" onClick={open} >Browse</button>
     <Button name="Online containers" click={ps} />
@@ -10,6 +10,7 @@ const Controls = ({ ps, psa, dcup, stop, fp, open, dcdwn }) => (
     <Button name="Compose up" click={dcup} />
     <Button name="Compose down" click={dcdwn} />
     <Button name="Stop containers" click={stop} />
+    <Button name="Saved directories" click={directories} />
   </div>
 );
 
