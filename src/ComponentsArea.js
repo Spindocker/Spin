f096aa9588b83794725cd121b492cc81adaa367b
jmangz <echou1@binghamton.edu>
2018-05-20 19:00:53 -0400

    added functionality

--- src/ComponentsArea.js
+++ src/ComponentsArea.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './ComponentsArea.css';
+import HeaderArea from './HeaderArea';
 
-const ComponentsArea = ({ comIds, currentViewName, filePath }) => (
+const ComponentsArea = ({ comIds, currentViewName, filePath, clear, directories }) => (
   <div>
     <div id="componentView">
+      <HeaderArea name={currentViewName} clear={clear} />
       <div id="headline">
         <div id="filePathDisplay"><p>Current Directory: {filePath}</p></div>
         <h1 id="title">{currentViewName}</h1>
       </div>
       {comIds}
+      <ul>{directories}</ul>
     </div>
   </div>
 );
