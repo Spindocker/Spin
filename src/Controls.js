54a1d251c3f5d89646a7afd29695b7641f4245d6
Haaris-S <haaris.ny@gmail.com>
2018-05-21 18:46:37 -0400

    control bar renames

--- src/Controls.js
+++ src/Controls.js
@@ -5,12 +5,12 @@ import './Controls.css';
 const Controls = ({ ps, psa, dcup, stop, fp, open, dcdwn, directories }) => (
   <div id="controlsBar">
     <button id="openFolder" onClick={open} >Browse</button>
-    <Button name="Online containers" click={ps} />
-    <Button name="All containers" click={psa} />
-    <Button name="Compose up" click={dcup} />
-    <Button name="Compose down" click={dcdwn} />
-    <Button name="Stop containers" click={stop} />
-    <Button name="Saved directories" click={directories} />
+    <Button name="Online / Composed Containers" click={ps} />
+    <Button name="All Containers" click={psa} />
+    <Button name="Compose Up" click={dcup} />
+    <Button name="Compose Down" click={dcdwn} />
+    <Button name="Stop Containers" click={stop} />
+    <Button name="Saved Directories" click={directories} />
   </div>
 );
 
