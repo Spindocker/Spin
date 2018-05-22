7ca91cd35ffc8f93e448c82a41dec8c6ec06959d
jmangz <echou1@binghamton.edu>
2018-05-22 13:10:29 -0400

    added images view

--- src/ComponentsArea.js
+++ src/ComponentsArea.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './ComponentsArea.css';
 import HeaderArea from './HeaderArea';
 
-const ComponentsArea = ({ comIds, currentViewName, filePath, clear, directories }) => (
+const ComponentsArea = ({ comIds, currentViewName, filePath, clear, directories, actionBtnClicked, showImages }) => (
   <div>
-    <HeaderArea name={currentViewName} clear={clear} />
+    <HeaderArea name={currentViewName} clear={clear} actionBtnClicked={actionBtnClicked} />
     <div id="headline">
       <div id="filePathDisplay"><p>Current Directory: {filePath}</p></div>
       <h2 id="title">{currentViewName}</h2>
     </div>
     <div id="componentView">
       {comIds}
+      {showImages}
       <ul id="directoryList">{directories}</ul>
     </div>
   </div>
