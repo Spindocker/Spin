de35ae88fd9ec5960d8f57a8c7386b93ffc0ede5
jmangz <echou1@binghamton.edu>
2018-05-30 22:09:38 -0400

    loading area and css

--- src/Console.js
+++ src/Console.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './Console.css';
 
 const Console = ({ loading }) => (
-    <div id="console">
-        {loading ? <p id="load-text">Loading...</p> : false}
+    <div>
+      { loading ? <div id="console"><p id="load-text">Loading...</p></div> : false }
     </div>
 );
 
